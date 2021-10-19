258596b11aee423194001fb4b563c34227e37f03
James Turner <james.turner01@trinitymirror.com>
2021-10-19 10:57:17 +0100

    Changing back to new eth alchemy key

--- networks.config.ts
+++ networks.config.ts
@@ -4,8 +4,8 @@ module.exports = {
     hardhat: {
         // allowUnlimitedContractSize: true,
         forking: {
-            // url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_API_KEY}`,
-            url: `https://mainnet.infura.io/v3/${process.env.INFURA_ETH_API_KEY}`,
+            url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_API_KEY}`,
+            // url: `https://mainnet.infura.io/v3/${process.env.INFURA_ETH_API_KEY}`,
             blockNumber: process.env.ETH_PINNED_BLOCK ? parseInt(process.env.ETH_PINNED_BLOCK) : 13433444
         }
     },
