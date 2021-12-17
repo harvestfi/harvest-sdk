a4c76dceee8541eedc515da69592021a80674ef3
James Turner <james.turner.phpninja@gmail.com>
2021-12-17 12:43:04 +0000

    Feature/unstake withdraw uni v3 support (#5)
    * Incorrect filename reference
    
    * Removed console log
    
    Co-authored-by: James Turner <james.turner01@trinitymirror.com>

--- src/strategies/withdrawals/iWithdrawalStrategy.ts
+++ src/strategies/withdrawals/iWithdrawalStrategy.ts
@@ -1,5 +1,4 @@
 import {BigNumber, ContractReceipt} from "ethers";
-import {IToken} from "../../tokens/token";
 
 export interface IWithdrawalStrategy {
     withdraw(amount: BigNumber): Promise<ContractReceipt>
